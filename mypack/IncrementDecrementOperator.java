0ecba2952e1fa3c1901f8fedfb38e681b77caf31
DYaminiManohar <manohar012007@gmail.com>
2025-08-02 11:11:41 +0530

    Update IncrementDecrementOperator.java

--- mypack/IncrementDecrementOperator.java
+++ mypack/IncrementDecrementOperator.java
@@ -1,5 +1,19 @@
 package mypack;
 
 public class IncrementDecrementOperator {
-	
+	public static void main(String[] args) {
+		// Demonstrate increment and decrement operators on a variable.
+		int a = 10;
+		a++;
+		System.out.println("Number after post increment: "+a);
+		
+		a--;
+		System.out.println("Number after post decrement: "+a);
+		
+		++a;
+		System.out.println("Number after pre increment: "+a);
+		
+		--a;
+		System.out.println("Number after pre decrement: "+a);
 }
+
