00860333db4396313267132285135b6068d0ccfa
DYaminiManohar <manohar012007@gmail.com>
2025-08-02 11:12:55 +0530

    Update ternaryoperator.java

--- mypack/ternaryoperator.java
+++ mypack/ternaryoperator.java
@@ -2,7 +2,7 @@ package mypack;
 import java.util.*;
 public class ternaryoperator {
 	public static void main(String[] args) {
-		// find the greater of two numbers using a ternary operator.
+	
          System.out.println("Enter two numbers");
          Scanner sc = new Scanner(System.in);
          int a = sc.nextInt();
@@ -13,4 +13,5 @@ public class ternaryoperator {
          
          
 	}
+
 }
